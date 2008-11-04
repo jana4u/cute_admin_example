f2cd57e4a6160943326bbabb1132fc2ae4b1a143
jana4u <jana4u@seznam.cz>
2008-11-04 15:42:32 +0100

    added has_many associations to models

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
+  has_many :employees
   acts_as_cute_admin
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :company
+  has_many :emails
+  has_many :phone_numbers
   acts_as_cute_admin :display_name => :full_name_with_number, :order_by => [:last_name, :first_name, :employee_number]
 
   def full_name_with_number
