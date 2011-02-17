78a46afc0d0864c46a41a94a46f2a49b38937d00
jana4u <jana4u@seznam.cz>
2011-02-17 17:33:32 +0100

    updated cute_admin submodule, removed Searchlogic patch, added test

--- lib/searchlogic_patches.rb
+++ lib/searchlogic_patches.rb
@@ -10,34 +10,3 @@ ActiveRecord::Associations::ClassMethods::InnerJoinDependency::InnerJoinAssociat
       "LEFT OUTER JOIN"
     end
 end
-
-
-# http://github.com/jana4u/searchlogic/commit/72c169f3f8aaed163f8c7f8f77a97efa9805dc0b
-
-module Searchlogic
-  module NamedScopes
-    module OrConditions
-      private
-        def or_conditions(name)
-          # First determine if we should even work on the name, we want to be as quick as possible
-          # with this.
-          begin
-            if (parts = split_or_condition(name)).size > 1
-              conditions = interpolate_or_conditions(parts)
-              if conditions.any?
-                conditions
-              else
-                nil
-              end
-            end
-          rescue Searchlogic::NamedScopes::OrConditions::UnknownConditionError
-            if respond_to?(name)
-              return nil
-            else
-              raise
-            end
-          end
-        end
-    end
-  end
-end
